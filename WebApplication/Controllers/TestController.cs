84a0fad29748a7bd3cd657f2af04d081341f355c
shenjl <selonsy@gmail.com>
2017-03-20 11:09:46 +0800

    Demo for Course

---
+++ WebApplication/Controllers/TestController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        #region MyRegion
+
+        /// <summary>
+        /// cookie名称
+        /// </summary>
+        private static string cookie_name = "mdata";
+        /// <summary>
+        /// cookie有效时间
+        /// </summary>
+        private static int cookie_expire = 20 * 60;
+
+        #endregion
+
+        /// <summary>
+        /// 获取电影列表
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult List(int type = 0)
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            if (type == 1 && !_is_login())
+            {
+                //校验是否登录
+                result.Data = new { code = 3, msg = "forbidden", info = "非法访问" };
+                return result;
+            }
+            result.Data = new
+            {
+                code = 1,
+                msg = "success",
+                info = "成功",
+                data = new[] {
+                        new { name="金刚狼3",director="路人甲" },
+                        new { name="速度与激情8",director="路人乙"},
+                        new { name="生化危机7",director="路人丙"}
+                    }
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult Login()
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            string username = Request.QueryString["username"];
+            string password = Request.QueryString["password"];
+            if (username == "admin" && password == "123456")
+            {
+                //保存登录信息
+                _save_cookie(username, password);
+                result.Data = new { code = 1, msg = "success", info = "登录成功" };
+            }
+            else
+            {
+                result.Data = new { code = 0, msg = "fail", info = "登录失败" };
+            }
+            return result;
+        }
+
+        #region private
+
+        /// <summary>
+        /// 保存cookie
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        private void _save_cookie(string username, string password)
+        {
+            HttpCookie cookie = Request.Cookies[cookie_name];
+            if (cookie == null)
+                cookie = new HttpCookie(cookie_name);
+            cookie.Value = username + ":" + password;
+            cookie.Expires = DateTime.Now.AddSeconds(cookie_expire);
+            Response.AppendCookie(cookie);
+        }
+
+        /// <summary>
+        /// 校验cookie
+        /// </summary>
+        private bool _is_login()
+        {
+            bool result = false;
+            HttpCookie cookie = Request.Cookies[cookie_name];
+            if (cookie != null && !string.IsNullOrEmpty(cookie.Value))
+            {
+                string[] cookie_values = cookie.Value.Split(':');
+                if (cookie_values.Length == 2 && cookie_values[0] == "admin" && cookie_values[1] == "123456")
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        #endregion
+    }
+
+    /// <summary> 
+    /// Bank Account demo class. 
+    /// </summary> 
+    public class BankAccount
+    {
+        #region 字段
+
+        //用户名
+        private readonly string _customerName;
+
+        //账户余额
+        private double _balance;
+
+        //账户状态
+        private bool _frozen = false;
+
+        //超限异常信息，自定义，用于获取具体抛出的异常
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount less than zero";
+
+        #endregion
+
+        #region 构造方法
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+        #endregion
+
+        #region 属性
+
+        public string CustomerName
+        {
+            get { return _customerName; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 取款
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Debit(double amount)
+        {
+            if (_frozen)
+            {
+                throw new Exception("您的账户被禁用了！");
+            }
+            if (amount > _balance)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+            _balance -= amount;
+        }
+
+        /// <summary>
+        /// 存款
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Credit(double amount)
+        {
+            if (_frozen)
+            {
+                throw new Exception("您的账户被禁用了！");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+            _balance += amount;
+        }
+
+        #region private
+
+        /// <summary>
+        /// 冻结账户
+        /// </summary>
+        private void FreezeAccount()
+        {
+            _frozen = true;
+        }
+
+        /// <summary>
+        /// 解冻账户
+        /// </summary>
+        private void UnfreezeAccount()
+        {
+            _frozen = false;
+        }
+
+        #endregion
+    }
+}
---
+++ WebApplicationTests/BankAccountTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using WebApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers.Tests
+{
+    [TestFixture()]
+    public class BankAccountTests
+    {
+        /// <summary>
+        /// 取款测试
+        /// </summary>
+        [Test()]
+        public void DebitTest()
+        {            
+            //创建一个账户devin，余额为100元
+            BankAccount bank = new BankAccount("devin", 100);
+            //取款10元
+            bank.Debit(10);
+
+            double expected = 90;
+            double actual = bank.Balance;
+
+            //Assert在这里可以理解成断言：在VS里做单元测试是基于断言的测试
+            //预计的结果是90元，如果等于实际的结果的话就通过，否则不通过。
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// 存款测试
+        /// </summary>
+        [Test()]
+        public void CreditTest()
+        {
+            //创建一个账户devin，余额为100元
+            BankAccount bank = new BankAccount("devin", 100);
+            //存款10元
+            bank.Credit(10);
+            //Assert在这里可以理解成断言：在VS里做单元测试是基于断言的测试
+            //预计的结果是110元，如果等于实际的结果的话就通过，否则不通过。
+            Assert.AreEqual(110, bank.Balance);
+        }
+        
+        /// <summary>
+        /// 测试Debit方法，如果取款金额小于0，那么应该抛出超限的异常
+        /// </summary>
+        [Test()]        
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("devin", beginningBalance);
+
+            // act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            //如果不进入catch的话，在不检查任何条件的情况下使断言失败，显示消息
+            Assert.Fail("No exception was thrown.");
+        }
+        
+        /// <summary>
+        /// 测试Debit方法，如果金额大于余额，那么应该抛出超限的异常
+        /// </summary>
+        [Test()]
+        public void Debit_WhenAmountIsGreaterThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 21.99;
+            double debitAmount = 22.0;
+            BankAccount account = new BankAccount("devin", beginningBalance);
+
+            // act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            //如果不进入catch的话，在不检查任何条件的情况下使断言失败，显示消息
+            Assert.Fail("No exception was thrown.");
+        }
+    }
+}
